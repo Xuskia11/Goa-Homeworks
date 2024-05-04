e2b1f6ecbd3e5a69b1e6541c5ab110390517cfba
Xuskia11 <atuka.09.123@gmail.com>
2024-05-04 18:06:47 +0400

    commit

---
+++ Day 36/classwork/codewars.py
@@ -0,0 +1,92 @@
+#Array plus array
+def array_plus_array(arr1,arr2):
+    
+    return sum(arr1) + sum(arr2)
+
+#Rock Paper Scissors!
+def rps(p1, p2):
+    if p1 == "scissors" and p2 == "paper":
+        return "Player 1 won!"
+    elif p1 == "paper" and p2 == "rock":
+        return "Player 1 won!"
+    elif p1 == "rock" and p2 == "scissors":
+         return "Player 1 won!"
+    
+    
+    
+    if p1 == "scissors" and p2 == "rock":
+        return "Player 2 won!"
+    elif p1 == "rock" and p2 == "paper":
+        return "Player 2 won!"
+    elif p1 == "paper" and p2 == "scissors":
+        return "Player 2 won!" 
+    elif p1 == p2:
+        return "Draw!"
+    
+#Area or perimeter
+def area_or_perimeter(l , w):
+    if l == w:
+        return l * w
+    else:
+        return (l + w) * 2
+    
+#Remove anchor from URL
+def remove_url_anchor(url):
+    x =  url.split("#")
+    return x[0]
+
+#Anagram Detection
+def is_anagram(test, original):
+    test = test.lower()
+    original = original.lower()
+    x = sorted(test)
+    y = sorted(original)
+    if x == y:
+        return True
+    else:
+        return False
+    
+#Greatest common divisor
+def mygcd(x, y):
+    while y > 0:
+        x, y = y, x % y
+    return x
+
+#Write Number in Expanded Form
+def expanded_form(num):
+    s = []
+    i = len(str(num)) -1
+    for n in str(num):
+        if n != "0":
+            s.append(n + "0" * i)
+        i -= 1
+    return " + ".join(s)
+
+#Collatz
+def collatz(n):
+    lists = [str(n)]
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        lists.append(str(int(n)))
+    return "->".join(lists)
+
+#Simple Pig Latin
+def pig_it(text):
+    
+    texts = text.split(" ")
+    word = ""
+    
+    for i in texts:
+        if i in "!%&.?":
+            word = word + i
+        else:
+            i= i[1:]+ i[0] + "ay"
+            word = word + i + " "
+            
+    return word.strip()
+
+
+
